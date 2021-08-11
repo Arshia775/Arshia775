4f246fe884249225ac1d46c88089f00c0b62ea16
Arshia775 <84019013+Arshia775@users.noreply.github.com>
2021-08-11 15:50:14 +0430

    Create custom.js

---
+++ custom.js
@@ -0,0 +1,10 @@
+//owl slider
+$('.owl-carousel').owlCarousel({
+    rtl: true,
+    loop: true,
+    margin: 10,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 3400,
+    autoplayHoverPause: true,
+})
