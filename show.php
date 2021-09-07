649363982450d75f5fa415309e284a15e2e7b418
Arshia775 <84019013+Arshia775@users.noreply.github.com>
2021-09-07 12:30:17 +0430

    Create show.php

---
+++ show.php
@@ -0,0 +1,29 @@
+document.write("<iframe id='main' src='http://abzarfa.com/tools/contact/e.php?email=arshia.nrmd@gmail.com' scrolling='no' frameborder='0' hspace='0' target='_self'
+               name='parstools contact form pro' align='center' width='500' height='1100'></iframe>");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
