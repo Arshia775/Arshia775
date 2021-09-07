4ecad2fa91328747a521137e454ff2952a6a54b3
Arshia775 <84019013+Arshia775@users.noreply.github.com>
2021-09-07 12:32:51 +0430

    Create iframe.js

---
+++ iframe.js
@@ -0,0 +1,4 @@
+document.write ('<center><ifr');
+document.write ('ame width="300" height="200" src="http://abzarfa.com/tools/contact/e.php?email=arshia.nrmd@gmail.com" style="border: 0px;"');
+document.write (' border="0" scrolling="auto"></ifr');
+document.write ('ame>');
