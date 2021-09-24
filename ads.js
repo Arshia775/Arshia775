92ec454f83ad89448c48ad566bfd91c932388858
Arshia775 <84019013+Arshia775@users.noreply.github.com>
2021-09-24 19:56:41 +0330

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,12 @@
+document.write ('(function(){
+    var now = new Date();
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.create');
+document.write ('Element('script');
+    script.async = true;
+    var script_address = 'https://cdn.yekt');
+document.write ('anet.com/js/soalpaye7.blogfa.com/native-soalpaye7.blogfa.com-20753.js';
+    script.src = script_address + '?v=' + now.getFullYear().toString() +');
+document.write (' '0' + now.getMonth() + '0' + now.getDate() + '0' + now.getHours();
+    head.appendChild(script);
+    })();');
